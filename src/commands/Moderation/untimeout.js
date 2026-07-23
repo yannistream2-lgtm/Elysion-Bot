92bdc2f91cf1a2c1156523390f6108c71cba99ab
yannistream2-lgtm <yanni.stream2@gmail.com>
2026-07-23 19:02:07 +0200

    Update untimeout.js

--- src/commands/Moderation/untimeout.js
+++ src/commands/Moderation/untimeout.js
@@ -6,61 +6,66 @@ import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName("untimeout")
-        .setDescription("Remove timeout from a user")
-        .addUserOption((option) =>
-            option
-                .setName("target")
-                .setDescription("User to untimeout")
-                .setRequired(true),
-        )
-        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
-    category: "moderation",
+data: new SlashCommandBuilder()
+.setName("untimeout")
+.setDescription("Retirer le timeout d'un utilisateur")
+.addUserOption((option) =>
+option
+.setName("target")
+.setDescription("Utilisateur dont le timeout doit être retiré")
+.setRequired(true),
+)
+.setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 
-    async execute(interaction, config, client) {
-        const deferSuccess = await InteractionHelper.safeDefer(interaction);
-        if (!deferSuccess) {
-            logger.warn(`Untimeout interaction defer failed`, {
-                userId: interaction.user.id,
-                guildId: interaction.guildId,
-                commandName: 'untimeout',
-            });
-            return;
-        }
+```
+category: "moderation",
 
-        const targetUser = interaction.options.getUser("target");
-        const member = interaction.options.getMember("target");
+async execute(interaction, config, client) {
+    const deferSuccess = await InteractionHelper.safeDefer(interaction);
 
-        if (!targetUser) {
-            throw new TitanBotError(
-                'Missing target user',
-                ErrorTypes.USER_INPUT,
-                'You must specify a user to untimeout.',
-                { subtype: 'invalid_user' },
-            );
-        }
-
-        if (!member) {
-            throw new TitanBotError(
-                "Target not found",
-                ErrorTypes.USER_INPUT,
-                "The target user is not currently in this server.",
-            );
-        }
-
-        await ModerationService.removeTimeoutUser({
-            guild: interaction.guild,
-            member,
-            moderator: interaction.member,
+    if (!deferSuccess) {
+        logger.warn(`Échec de la réponse différée de l'interaction untimeout`, {
+            userId: interaction.user.id,
+            guildId: interaction.guildId,
+            commandName: 'untimeout',
         });
+        return;
+    }
 
-        await InteractionHelper.safeEditReply(interaction, {
-            embeds: [
-                successEmbed(
-                    `🔓 **Removed timeout** from ${targetUser.tag}`,
-                ),
-            ],
-        });
-    },
+    const targetUser = interaction.options.getUser("target");
+    const member = interaction.options.getMember("target");
+
+    if (!targetUser) {
+        throw new TitanBotError(
+            'Utilisateur cible manquant',
+            ErrorTypes.USER_INPUT,
+            'Vous devez spécifier un utilisateur dont le timeout doit être retiré.',
+            { subtype: 'invalid_user' },
+        );
+    }
+
+    if (!member) {
+        throw new TitanBotError(
+            "Cible introuvable",
+            ErrorTypes.USER_INPUT,
+            "L'utilisateur cible n'est actuellement pas présent sur ce serveur.",
+        );
+    }
+
+    await ModerationService.removeTimeoutUser({
+        guild: interaction.guild,
+        member,
+        moderator: interaction.member,
+    });
+
+    await InteractionHelper.safeEditReply(interaction, {
+        embeds: [
+            successEmbed(
+                `🔓 **Timeout retiré** de ${targetUser.tag}`,
+            ),
+        ],
+    });
+},
+```
+
 };
