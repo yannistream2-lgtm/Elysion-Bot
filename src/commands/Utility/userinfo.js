9e5d544a8d8d64abc9eb0d8bebeca06cb7e73ccb
yannistream2-lgtm <yanni.stream2@gmail.com>
2026-07-23 19:41:10 +0200

    Update userinfo.js

--- src/commands/Utility/userinfo.js
+++ src/commands/Utility/userinfo.js
@@ -2,71 +2,90 @@ import { SlashCommandBuilder } from 'discord.js';
 import { createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
     data: new SlashCommandBuilder()
-    .setName("userinfo")
-    .setDescription("Get detailed information about a user")
-    .addUserOption((option) =>
-      option
-        .setName("target")
-        .setDescription("The user to inspect (defaults to you)"),
-    ),
+        .setName("userinfo")
+        .setDescription("Obtenir des informations détaillées sur un utilisateur")
+        .addUserOption((option) =>
+            option
+                .setName("target")
+                .setDescription("L'utilisateur à inspecter (vous-même par défaut)"),
+        ),
 
-  async execute(interaction) {
-    const deferSuccess = await InteractionHelper.safeDefer(interaction);
-    if (!deferSuccess) {
-      logger.warn(`UserInfo interaction defer failed`, {
-        userId: interaction.user.id,
-        guildId: interaction.guildId,
-        commandName: 'userinfo'
-      });
-      return;
-    }
+    async execute(interaction) {
+        const deferSuccess = await InteractionHelper.safeDefer(interaction);
 
-    const user = interaction.options.getUser("target") || interaction.user;
-    const member = interaction.guild.members.cache.get(user.id);
+        if (!deferSuccess) {
+            logger.warn(`Échec du report de l'interaction UserInfo`, {
+                userId: interaction.user.id,
+                guildId: interaction.guildId,
+                commandName: 'userinfo'
+            });
+            return;
+        }
 
-    const createdTimestamp = Math.floor(user.createdAt.getTime() / 1000);
-    const joinedTimestamp = member?.joinedAt ? Math.floor(member.joinedAt.getTime() / 1000) : null;
+        const user = interaction.options.getUser("target") || interaction.user;
+        const member = interaction.guild.members.cache.get(user.id);
 
-    const embed = createEmbed({ title: `User Info: ${user.username}` })
-      .setThumbnail(user.displayAvatarURL({ size: 256 }))
-      .addFields(
-        { name: "ID", value: user.id, inline: true },
-        { name: "Bot", value: user.bot ? "Yes" : "No", inline: true },
-        {
-          name: "Roles",
-          value:
-            member && member.roles.cache.size > 1
-              ? member.roles.cache
-                  .map((r) => r.name)
-                  .slice(0, 5)
-                  .join(",")
-              : "None",
-          inline: true,
-        },
-        {
-          name: "Account Created",
-          value: `<t:${createdTimestamp}:R>`,
-          inline: false,
-        },
-        {
-          name: "Joined Server",
-          value: joinedTimestamp ? `<t:${joinedTimestamp}:R>` : "Not in server",
-          inline: false,
-        },
-        {
-          name: "Highest Role",
-          value: member?.roles?.highest?.name || "None",
-          inline: true,
-        },
-      );
+        const createdTimestamp = Math.floor(user.createdAt.getTime() / 1000);
+        const joinedTimestamp = member?.joinedAt
+            ? Math.floor(member.joinedAt.getTime() / 1000)
+            : null;
 
-    await InteractionHelper.safeEditReply(interaction, { embeds: [embed] });
-    logger.info(`UserInfo command executed`, {
-      userId: interaction.user.id,
-      targetUserId: user.id,
-      guildId: interaction.guildId
-    });
-  },
+        const embed = createEmbed({
+            title: `Informations sur l'utilisateur : ${user.username}`
+        })
+            .setThumbnail(user.displayAvatarURL({ size: 256 }))
+            .addFields(
+                {
+                    name: "ID",
+                    value: user.id,
+                    inline: true
+                },
+                {
+                    name: "Bot",
+                    value: user.bot ? "Oui" : "Non",
+                    inline: true
+                },
+                {
+                    name: "Rôles",
+                    value:
+                        member && member.roles.cache.size > 1
+                            ? member.roles.cache
+                                .map((r) => r.name)
+                                .slice(0, 5)
+                                .join(", ")
+                            : "Aucun",
+                    inline: true,
+                },
+                {
+                    name: "Compte créé",
+                    value: `<t:${createdTimestamp}:R>`,
+                    inline: false,
+                },
+                {
+                    name: "A rejoint le serveur",
+                    value: joinedTimestamp
+                        ? `<t:${joinedTimestamp}:R>`
+                        : "N'est pas membre du serveur",
+                    inline: false,
+                },
+                {
+                    name: "Rôle le plus élevé",
+                    value: member?.roles?.highest?.name || "Aucun",
+                    inline: true,
+                },
+            );
+
+        await InteractionHelper.safeEditReply(interaction, {
+            embeds: [embed]
+        });
+
+        logger.info(`Commande UserInfo exécutée`, {
+            userId: interaction.user.id,
+            targetUserId: user.id,
+            guildId: interaction.guildId
+        });
+    },
 };
