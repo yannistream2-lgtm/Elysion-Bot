aad1c92d9042f486f54b4093de68906a94af928d
yannistream2-lgtm <yanni.stream2@gmail.com>
2026-07-23 18:43:53 +0200

    Update levelremove.js

--- src/commands/Leveling/levelremove.js
+++ src/commands/Leveling/levelremove.js
@@ -6,25 +6,27 @@ import { removeLevels, getUserLevelData, getLevelingConfig } from '../../service
 import { createEmbed } from '../../utils/embeds.js';
 
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
   data: new SlashCommandBuilder()
-    .setName('levelremove')
-    .setDescription('Remove levels from a user')
+    .setName('retirerniveaux')
+    .setDescription('Retirer des niveaux à un utilisateur')
     .addUserOption((option) =>
       option
-        .setName('user')
-        .setDescription('The user to remove levels from')
+        .setName('utilisateur')
+        .setDescription('L’utilisateur auquel retirer des niveaux')
         .setRequired(true)
     )
     .addIntegerOption((option) =>
       option
-        .setName('levels')
-        .setDescription('Number of levels to remove')
+        .setName('niveaux')
+        .setDescription('Nombre de niveaux à retirer')
         .setRequired(true)
         .setMinValue(1)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
+
   category: 'Leveling',
 
   async execute(interaction, config, client) {
@@ -33,58 +35,73 @@ export default {
     const hasPermission = await checkUserPermissions(
       interaction,
       PermissionFlagsBits.ManageGuild,
-      'You need ManageGuild permission to use this command.'
+      'Vous devez avoir la permission **Gérer le serveur** pour utiliser cette commande.'
     );
+
     if (!hasPermission) return;
 
     const levelingConfig = await getLevelingConfig(client, interaction.guildId);
+
     if (!levelingConfig?.enabled) {
       await InteractionHelper.safeEditReply(interaction, {
         embeds: [
           new EmbedBuilder()
             .setColor('#f1c40f')
-            .setDescription('The leveling system is currently disabled on this server.')
+            .setDescription('Le système de niveaux est actuellement désactivé sur ce serveur.')
         ],
         flags: MessageFlags.Ephemeral
       });
       return;
     }
 
-    const targetUser = interaction.options.getUser('user');
-    const levelsToRemove = interaction.options.getInteger('levels');
+    const targetUser = interaction.options.getUser('utilisateur');
+    const levelsToRemove = interaction.options.getInteger('niveaux');
 
-    const member = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
+    const member = await interaction.guild.members
+      .fetch(targetUser.id)
+      .catch(() => null);
+
     if (!member) {
       throw new TitanBotError(
-        `User ${targetUser.id} not found in this guild`,
+        `Utilisateur ${targetUser.id} introuvable sur ce serveur`,
         ErrorTypes.USER_INPUT,
-        'The specified user is not in this server.'
+        'L’utilisateur spécifié ne fait pas partie de ce serveur.'
       );
     }
 
-    const userData = await getUserLevelData(client, interaction.guildId, targetUser.id);
+    const userData = await getUserLevelData(
+      client,
+      interaction.guildId,
+      targetUser.id
+    );
+
     if (userData.level === 0) {
       throw new TitanBotError(
-        `User ${targetUser.id} is already at minimum level`,
+        `Utilisateur ${targetUser.id} déjà au niveau minimum`,
         ErrorTypes.VALIDATION,
-        `${targetUser.tag} is already at level 0 and cannot have levels removed.`
+        `${targetUser.tag} est déjà au niveau 0 et aucun niveau supplémentaire ne peut lui être retiré.`
       );
     }
 
-    const updatedData = await removeLevels(client, interaction.guildId, targetUser.id, levelsToRemove);
+    const updatedData = await removeLevels(
+      client,
+      interaction.guildId,
+      targetUser.id,
+      levelsToRemove
+    );
 
     await InteractionHelper.safeEditReply(interaction, {
       embeds: [
         createEmbed({
-          title: 'Levels Removed',
-          description: `Successfully removed ${levelsToRemove} levels from ${targetUser.tag}.\n**New Level:** ${updatedData.level}`,
+          title: 'Niveaux retirés',
+          description: `**${levelsToRemove}** niveau(x) ont été retirés avec succès à ${targetUser.tag}.\n**Nouveau niveau :** ${updatedData.level}`,
           color: 'success'
         })
       ]
     });
 
     logger.info(
-      `[ADMIN] User ${interaction.user.tag} removed ${levelsToRemove} levels from ${targetUser.tag} in guild ${interaction.guildId}`
+      `[ADMIN] L'utilisateur ${interaction.user.tag} a retiré ${levelsToRemove} niveau(x) à ${targetUser.tag} dans le serveur ${interaction.guildId}`
     );
   }
 };
